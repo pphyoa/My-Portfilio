5216b07278689b956f31dd92754ca59035872792
Pyae Phyo Aung <pphyoa65@gmail.com>
2022-07-25 14:05:09 +0630

    Create My Portfilio

---
+++ js/style.js
@@ -0,0 +1,172 @@
+window.sr=ScrollReveal();
+        sr.reveal('.headtitle',{
+            origin : 'bottom',
+            distance: '30px',
+            duration : 800 ,
+        });
+
+        sr.reveal('.headpage',{
+            origin : 'bottom',
+            distance: '30px',
+            duration : 800 ,
+            delay :500,
+        }); 
+
+        sr.reveal('.webdev',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :900,
+        }); 
+        sr.reveal('.webimgdev',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :900,
+        }); 
+        sr.reveal('.pro-1',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        }); 
+        sr.reveal('.pro-2',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :600,
+        }); 
+        sr.reveal('.pro-3',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :700,
+        }); 
+        sr.reveal('.uni-title',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        }); 
+        sr.reveal('.carousel',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        }); 
+        sr.reveal('.con1',{
+            origin : 'left',
+            distance: '200px',
+            duration : 800 ,
+            delay :900,
+        }); 
+        sr.reveal('.con2',{
+            origin : 'left',
+            distance: '200px',
+            duration : 800 ,
+            delay :700,
+        }); 
+        sr.reveal('.con3',{
+            origin : 'left',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        }); 
+        sr.reveal('.icons-box',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        }); 
+        sr.reveal('.css-box',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :650,
+        }); 
+        sr.reveal('.bootstrap-box',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :750,
+        }); 
+        sr.reveal('.js-box',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        }); 
+        sr.reveal('.php-box',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :650,
+        }); 
+        sr.reveal('.laravel-box',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :750,
+        }); 
+        sr.reveal('.sigbox',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        });
+        sr.reveal('.sigin',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :600,
+        });
+        sr.reveal('.form1',{
+            origin : 'left',
+            distance: '200px',
+            duration : 800 ,
+            delay :700,
+        });
+        sr.reveal('.form2',{
+            origin : 'left',
+            distance: '200px',
+            duration : 800 ,
+            delay :800,
+        });
+        sr.reveal('.form3',{
+            origin : 'left',
+            distance: '200px',
+            duration : 800 ,
+            delay :900,
+        });
+        sr.reveal('.footer1',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        });
+        sr.reveal('.footer2',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :650,
+        });
+        sr.reveal('.footer3',{
+            origin : 'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :800,
+        });
+        sr.reveal('.footend',{
+     
+            origin :'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :500,
+        });
+        sr.reveal('.uni-block',{
+     
+            origin :'bottom',
+            distance: '200px',
+            duration : 800 ,
+            delay :800,
+        });
